71df1a0dee4e7f74aedb0fab0895413b3a531dc3
romaa <nahalevskyi_ak17@nuwm.edu.ua>
2021-08-25 10:38:53 +0300

    feat: Created custom map marker

---
+++ components/UserMarker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-marker">
+    <div class="user-marker__circle">
+      <div class="user-marker__avatar">
+        <img :src="img" alt="" />
+      </div>
+    </div>
+    <div class="user-marker__triangle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped></style>
