c4b97a12131809fa3a48b38d850d8a39e0010ac4
mogu4iy <mogu4iy.kotygoroshko@gmail.com>
2021-10-31 17:06:31 +0200

    feat: Add first free day for male, location request

--- components/modals/QuickMeetingsModal.vue
+++ components/modals/QuickMeetingsModal.vue
@@ -1,51 +1,53 @@
 <template>
-  <div class="overlay" v-if="qDateOff">
-  <div class="modal quick-meeting-modal">
+  <div class="overlay" v-if="qDateOff && coins.coins">
+    <div class="modal quick-meeting-modal">
      <span class="close" @click="$router.push(localePath('/profile'))">
       <inline-svg src="icons/close-modal.svg"/>
     </span>
 
-    <div v-if="topUp">
-      <form
-        class="ballance-card-block"
-        method="POST"
-        accept-charset="utf-8"
-        action="https://www.liqpay.ua/api/3/checkout"
-      >
-        <input type="hidden" name="data" :value="quickMeetingsLiqpay.data" />
-        <input type="hidden" name="signature" :value="quickMeetingsLiqpay.signature" />
-        <div class="row jc-center">
-          <ProfileCard :title="1" :price="0.99" :activeItem="activeItem" @setCheckCard="setCheckCard" />
-          <ProfileCard :title="10" :price="8.99" :activeItem="activeItem" @setCheckCard="setCheckCard" />
-          <ProfileCard :title="50" :price="32.99" :activeItem="activeItem" @setCheckCard="setCheckCard" />
-          <ProfileCard :title="100" :price="54.99" :activeItem="activeItem" @setCheckCard="setCheckCard" />
-        </div>
-        <button class="button button__full">{{$t('Pay with')}} <LiqpayIcon/></button>
-      </form>
-    </div>
+      <div v-if="topUp">
+        <form
+          class="ballance-card-block"
+          method="POST"
+          accept-charset="utf-8"
+          action="https://www.liqpay.ua/api/3/checkout"
+        >
+          <input type="hidden" name="data" :value="quickMeetingsLiqpay.data"/>
+          <input type="hidden" name="signature" :value="quickMeetingsLiqpay.signature"/>
+          <div class="row jc-center">
+            <ProfileCard :title="1" :price="0.99" :activeItem="activeItem" @setCheckCard="setCheckCard"/>
+            <ProfileCard :title="10" :price="8.99" :activeItem="activeItem" @setCheckCard="setCheckCard"/>
+            <ProfileCard :title="50" :price="32.99" :activeItem="activeItem" @setCheckCard="setCheckCard"/>
+            <ProfileCard :title="100" :price="54.99" :activeItem="activeItem" @setCheckCard="setCheckCard"/>
+          </div>
+          <button class="button button__full">{{ $t('Pay with') }}
+            <LiqpayIcon/>
+          </button>
+        </form>
+      </div>
 
-    <div class="modal-body" v-else>
-      <h2 class="title">{{$t('Fast dates')}}</h2>
-      <p class="text">{{$t('Make an appointment now.')}}</p>
-      <span class="quick-meeting-modal__icon">
+      <div class="modal-body" v-else>
+        <h2 class="title">{{ $t('Fast dates') }}</h2>
+        <p class="text">{{ $t('Make an appointment now.') }}</p>
+        <span class="quick-meeting-modal__icon">
        <inline-svg src="icons/quick-meetings-modal.svg"/>
      </span>
-      <p class="quick-meeting-modal__description">{{$t('Chat with users who are ready to meet live.')}}</p>
-      <!--button class="button button__full" @click="$emit('next')">Войти</button-->
-      <div>
-        <input type="hidden" name="data" :value="quickMeetingsLiqpay.data" />
-        <input type="hidden" name="signature" :value="quickMeetingsLiqpay.signature" />
-        <button
-          class="button button__full liqpay-btn"
-          @click="topUpBalance"
-          onmouseover="this.style.opacity='0.5';"
-          onmouseout="this.style.opacity='1';">
-          <img src="https://static.liqpay.ua/buttons/logo-small.png" name="btn_text" class="liqpay-img"/>
-          <span>{{$t('Buy coins')}}</span>
-        </button>
+        <p class="quick-meeting-modal__description">{{ $t('Chat with users who are ready to meet live.') }}</p>
+        <!--button class="button button__full" @click="$emit('next')">Войти</button-->
+        <div>
+          <input type="hidden" name="data" :value="quickMeetingsLiqpay.data"/>
+          <input type="hidden" name="signature" :value="quickMeetingsLiqpay.signature"/>
+          <button
+            class="button button__full liqpay-btn"
+            @click="topUpBalance"
+            onmouseover="this.style.opacity='0.5';"
+            onmouseout="this.style.opacity='1';">
+            <img src="https://static.liqpay.ua/buttons/logo-small.png" name="btn_text" class="liqpay-img"/>
+            <span>{{ $t('Buy coins') }}</span>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -53,12 +55,12 @@
 import LiqpayIcon from '@/static/icons/liqpay.svg'
 
 export default {
-  name:'SentPasswordModal',
+  name: 'SentPasswordModal',
   components: {
     LiqpayIcon
   },
-  props:{
-    modal:{
+  props: {
+    modal: {
       type: Object,
       required: true
     }
@@ -80,7 +82,9 @@ export default {
     },
     qDateOff() {
       let showWindow = true
-      if (this.coins.coins <= 0) {
+      let days = (Date.parse(new Date()) - Date.parse(this.getUserData.createdAt)) / 86400000;
+      console.log(this.coins.coins, days)
+      if (this.coins.coins <= 0 && days > 0) {
         showWindow = true
       } else if (this.$ws.store.qDateOff.length) {
         showWindow = this.getUserData.profile.gender !== "female";
@@ -94,15 +98,15 @@ export default {
     topUpBalance() {
       this.topUp = true
     },
-    async setCheckCard(card){
+    async setCheckCard(card) {
       this.activeItem = card.name
-      await this.$store.dispatch('quick-dating/generateLiqpay', { "amount" : card.title });
+      await this.$store.dispatch('quick-dating/generateLiqpay', {"amount": card.title});
     },
   },
-  async created(){
-    await this.$store.dispatch('quick-dating/generateLiqpay', { "amount" : 10 });
+  async created() {
+    await this.$store.dispatch('quick-dating/generateLiqpay', {"amount": 10});
     if (this.coins.coins > 0) {
-      await this.$store.dispatch("quick-dating/wantQdate", { "status": this.modal.show });
+      await this.$store.dispatch("quick-dating/wantQdate", {"status": this.modal.show});
     }
   }
 }
@@ -113,10 +117,12 @@ export default {
 [v-cloak] {
   display: none;
 }
-.liqpay-btn{
+
+.liqpay-btn {
   min-width: 250px;
 }
-.liqpay-img{
+
+.liqpay-img {
   margin-right: 7px !important;
 }
 </style>
--- store/quick-dating.js
+++ store/quick-dating.js
@@ -2,57 +2,67 @@ import {load} from '@/plugins/api';
 import ws from '../plugins/ws'
 
 export const state = () => ({
-  quickMeetingsPeoples:[],
+  quickMeetingsPeoples: [],
   quickMeetingsLiqPay: {},
   wantDate: null
 })
 
 export const actions = {
-  async fetchAllQuickMeetingsPeoples(ctx, payload){
+  async fetchAllQuickMeetingsPeoples(ctx, payload) {
     const token = localStorage.getItem('token');
-    //await fetch(`https://${process.env.API_HOST}/v2/quick-dating/select/want-qdate/near/all`, {
-    await fetch(`https://${process.env.API_HOST}/v2/user/select/near/all`, {
-      method: 'post',
+    await fetch(`https://${process.env.API_HOST}/v2/user/location`, {
+      method: 'put',
       headers: {
         'Authorization': `Bearer ${token}`,
         'geo-location': payload,
         'geo-reversed': true
-      },
+      }
     }).then(res => {
-      return res.json()
-    }).then(data => {
-      if(!data.err){
-        ctx.commit('quick-dating/setQuickMeetingsPeoples', data, {root: true})
-      }      
-    }).catch((e) => {
-      console.log('error', e);
+      console.log(res.status)
+      return fetch(`https://${process.env.API_HOST}/v2/user/select/near/all`, {
+        method: 'post',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'geo-location': payload,
+          'geo-reversed': true
+        },
+      })
     })
+      .then(res => {
+        return res.json()
+      }).then(data => {
+        if (!data.err) {
+          ctx.commit('quick-dating/setQuickMeetingsPeoples', data, {root: true})
+        }
+      }).catch((e) => {
+        console.log('error', e);
+      })
   },
 
-  async wantQdate({ commit }, payload) {
+  async wantQdate({commit}, payload) {
     await load("/v2/quick-dating/want-qdate", "put", payload, true)
       .then(data => {
-        commit("quick-dating/setQDate", data.status, { root: true })
+        commit("quick-dating/setQDate", data.status, {root: true})
       }).catch(err => {
         console.error(err)
       })
   },
 
   async generateLiqpay(ctx, payload) {
-    await load('/v2/coin/liqpay','put', payload, true)
-    .then(data => {
-      ctx.commit('quick-dating/setQuickMeetingsLiqpay', data, {root: true})
-    }).catch(e => {
-      throw e;
-    })
+    await load('/v2/coin/liqpay', 'put', payload, true)
+      .then(data => {
+        ctx.commit('quick-dating/setQuickMeetingsLiqpay', data, {root: true})
+      }).catch(e => {
+        throw e;
+      })
   },
 }
 
 export const mutations = {
-  setQuickMeetingsPeoples(state, payload){
+  setQuickMeetingsPeoples(state, payload) {
     state.quickMeetingsPeoples = payload
   },
-  setQuickMeetingsLiqpay(state, payload){
+  setQuickMeetingsLiqpay(state, payload) {
     state.quickMeetingsLiqPay = payload
   },
   setQDate(state, payload) {
@@ -61,10 +71,10 @@ export const mutations = {
 }
 
 export const getters = {
-  getQuickMeetingsPeoples(state){
+  getQuickMeetingsPeoples(state) {
     return state.quickMeetingsPeoples
   },
-  getQuickMeetingsLiqpay(state){
+  getQuickMeetingsLiqpay(state) {
     return state.quickMeetingsLiqPay
   }
 }
