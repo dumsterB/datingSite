0088f60ef3dcc22d985a60432e867a574c22b2bb
Behruz <74446521+dumsterB@users.noreply.github.com>
2021-10-31 21:30:57 +0500

    fix localization

--- plugins/modules.js
+++ plugins/modules.js
@@ -58,7 +58,10 @@ Vue.use(Core);
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyAsnLEOI5WAr6rpkBzsP8UVw3o4rRvx-70',
-    libraries: 'places', // This is required if you use the Autocomplete plugin
+    libraries: 'places', 
+    region: 'UA',
+    language: 'ua',
+    // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
     // (as you require)
